fbcc39e83f33f333fc861a7cdd7fc6c9849ea286
SanketTops <sanket.tops@gmail.com>
2022-12-30 16:35:45 +0530

    DBApp

---
+++ Django Project/DBProject/myapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class usersignup(models.Model):
+    firstname=models.CharField(max_length=20)
+    lastname=models.CharField(max_length=20)
+    email=models.EmailField()
+    password=models.CharField(max_length=12)
+    dob=models.DateField()
+    mobile=models.IntegerField()
+    address=models.TextField()
