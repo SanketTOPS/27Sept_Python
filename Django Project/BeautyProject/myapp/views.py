e282344044577def298a00d9d058a85e61c1c186
SanketTops <sanket.tops@gmail.com>
2023-01-20 15:57:28 +0530

    TemplateInte

---
+++ Django Project/BeautyProject/myapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def client(request):
+    return render(request,'client.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def products(request):
+    return render(request,'products.html')
